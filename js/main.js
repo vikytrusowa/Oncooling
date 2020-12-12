2a59b28757cada53348ec5dc6f54c9fc04f44bd6
logo-outsourcing <logo.outsors@gmail.com>
2020-12-12 11:12:16 +0200

    add slider

--- js/main.js
+++ js/main.js
@@ -1,25 +1,47 @@
-$('#carousel-example').on('slide.bs.carousel', function (e) {
-    /*
-        CC 2.0 License Iatek LLC 2018 - Attribution required
-    */
-    var $e = $(e.relatedTarget);
-    var idx = $e.index();
-    var itemsPerSlide = 5;
-    var totalItems = $('.carousel-item').length;
-
-    if (idx >= totalItems-(itemsPerSlide-1)) {
-        var it = itemsPerSlide - (totalItems - idx);
-        for (var i=0; i<it; i++) {
-            // append slides to end
-            if (e.direction=="left") {
-                $('.carousel-item').eq(i).appendTo('.carousel-inner');
-            }
-            else {
-                $('.carousel-item').eq(0).appendTo('.carousel-inner');
-            }
-        }
+var swiper = new Swiper('.activity__slider', {
+    slidesPerView: 4,
+    spaceBetween: 21,
+    slidesPerGroup: 1,
+    loop: true,
+    nextButton: '.activity__next',
+    prevButton: '.activity__prev',
+    breakpoints: {
+        480: {}
     }
 });
-$('#carousel-example').carousel({
-    interval: 50000
+var swiper2 = new Swiper('.news__slider', {
+    slidesPerView: 4,
+    spaceBetween: 21,
+    slidesPerGroup: 1,
+    loop: true,
+    nextButton: '.news__next',
+    prevButton: '.news__prev',
+    breakpoints: {
+        480: {}
+    }
 });
+// $('#carousel-example').on('slide.bs.carousel', function (e) {
+//     /*
+//         CC 2.0 License Iatek LLC 2018 - Attribution required
+//     */
+//     var $e = $(e.relatedTarget);
+//     var idx = $e.index();
+//     var itemsPerSlide = 5;
+//     var totalItems = $('.carousel-item').length;
+//
+//     if (idx >= totalItems-(itemsPerSlide-1)) {
+//         var it = itemsPerSlide - (totalItems - idx);
+//         for (var i=0; i<it; i++) {
+//             // append slides to end
+//             if (e.direction=="left") {
+//                 $('.carousel-item').eq(i).appendTo('.carousel-inner');
+//             }
+//             else {
+//                 $('.carousel-item').eq(0).appendTo('.carousel-inner');
+//             }
+//         }
+//     }
+// });
+// $('#carousel-example').carousel({
+//     interval: 50000
+// });
